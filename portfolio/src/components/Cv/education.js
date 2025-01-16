d283a32a9271d980c5e786e084377d273dcb8f0e
Maria Salonen <measalonen@gmail.com>
2025-01-16 15:29:58 +0200

    CV section

--- portfolio/src/components/Cv/education.js
+++ portfolio/src/components/Cv/education.js
@@ -1,10 +1,63 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Education = () => {
   return (
     <>
-      <Container>This is Education component inside a container</Container>
+      <Container className="cv-container">
+        {" "}
+        <div className="cv-item">
+          <Row className="cv-item-header">
+            <Col>Insinööri, Tieto- ja viestintätekniikka</Col>
+            <Col>2017 - 2020</Col>
+          </Row>
+          <p className="cv-item-header cv-prof-title">Mediatekniikka</p>
+          <p className="cv-item-text">Jyväskylän Ammattikorkeakoulu</p>
+        </div>
+        <div className="cv-item">
+          <Row className="cv-item-header">
+            <Col>Puutarhuri</Col>
+            <Col>2010 - 2013</Col>
+          </Row>
+          <p className="cv-item-text">Jämsän Ammattiopisto</p>
+        </div>
+        <div className="cv-item">
+          <Row className="cv-item-header">
+            <Col>Ylioppilas</Col>
+            <Col>2002 - 2005</Col>
+          </Row>
+          <p className="cv-item-text">Cygnaeus-lukio</p>
+        </div>
+     
+        <div className="cv-item">
+          <Row className="cv-item-header">
+            <Col>Scrum With User Experience I</Col>
+            <Col>2024</Col>
+          </Row>
+          <p className="cv-item-text">Scrum.org</p>
+        </div>
+        <div className="cv-item">
+          <Row className="cv-item-header">
+            <Col>Certifed SAFe 6 Scrum Master</Col>
+            <Col>2023</Col>
+          </Row>
+          <p className="cv-item-text">Scaled Agile</p>
+        </div>
+        <div className="cv-item">
+          <Row className="cv-item-header">
+            <Col>Certifed Scrum Master</Col>
+            <Col>2022</Col>
+          </Row>
+          <p className="cv-item-text">Scrum Alliance</p>
+        </div>
+        <div className="cv-item">
+          <Row className="cv-item-header">
+            <Col>Johdatus Palvelumuotoiluun</Col>
+            <Col>2024</Col>
+          </Row>
+          <p className="cv-item-text">Lapin Yliopisto</p>
+        </div>
+      </Container>
     </>
   );
 };
--- portfolio/src/components/Cv/skills.js
+++ portfolio/src/components/Cv/skills.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Skills = () => {
   return (
     <>
-      <Container>This is Skills component inside a container</Container>
+      <Container className="cv-container cv-item">
+        <Row className="cv-item-header skills-row">
+          <Col>
+            <div className="skills-header">TAIDOT</div>
+            <p className="cv-item-text">
+              Fasilitointi <br></br>
+              Ihmissuhdetaidot <br></br>
+              Organisointitaidot <br></br>
+              SAFe <br></br>
+              Scrum
+            </p>
+            <Col>
+            <div className="skills-header">KIELITAITO</div>
+            <p className="cv-item-text">
+              Suomi - Äidinkieli <br></br>
+              Englanti - Sujuva <br></br>
+              Ruotsi - Perusteet
+            </p>
+          </Col>
+          </Col>
+ 
+          <Col className="skills-right-col">
+            <div className="skills-header">TEKNOLOGIAT</div>
+            <p className="cv-item-text">
+              React <br></br>TypeScript <br></br>HTML <br></br>CSS <br></br>
+              JavaScript
+            </p>
+            <div className="skills-header">TYÖKALUT</div>
+            <p className="cv-item-text">
+              Figma <br></br> Adobe XD <br></br>GitHub & GitLab <br></br>Azure
+              Devops <br></br>Microsoft Office -työkalut
+            </p>
+          </Col>
+        </Row>
+        
+      </Container>
     </>
   );
 };
